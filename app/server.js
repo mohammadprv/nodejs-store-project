46cb5ddc336202d008d92475e5c4d339fab921e0
mohammadprv <mohammadpeyravi11@gmail.com>
2023-02-08 17:32:52 +0330

    Create First Application Config

---
+++ app/server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+
+module.exports = class Application {
+
+    #app = express();
+    #PORT;
+    #DB_URI;
+    constructor(PORT, DB_URI) {
+        this.#PORT = PORT;
+        this.#DB_URI = DB_URI;
+        this.configApplication();
+        this.connectToMongoDB();
+        this.createServer();
+        this.createRoutes();
+        this.errorHandler();
+    }
+
+    configApplication(){
+        this.#app.use(express.json());
+        this.#app.use(express.urlencoded({ extended: true }));
+        this.#app.use(express.static(path.join(__dirname, "..", "public")));
+    }
+
+    createServer() {
+        const http = require('http');
+        http.createServer(this.#app).listen(this.#PORT, () => {
+            console.log(`Server Is Up And Online On Port: ${ this.#PORT }`);     
+        })
+    }
+
+    connectToMongoDB() {
+        mongoose.set("strictQuery", true);
+        mongoose.connect(this.#DB_URI, (error) => {
+            if (!error) return console.log("Connected To MongoDB");
+            return console.log("Failed To Connect MongoDB");
+        })
+    }
+
+    createRoutes() {
+        
+    }
+
+    errorHandler() {
+        this.#app.use((req, res, next) => {
+            return res.status(404).json({
+                statusCode: 404,
+                message: "آدرس یا صفحه مورد نظر یافت نشد"
+            })
+        })
+
+        this.#app.use((err, req, res, next) => {
+            const statusCode = err.status || 500;
+            const message = err.message || "Internal Server Error";
+            return res.status(statusCode).json({
+                statusCode,
+                message
+            })
+        })
+
+    }
+
+}
