facdd1fe04021bbbbc35726dcb3040f62afd8023
mohammadprv <mohammadpeyravi11@gmail.com>
2023-02-11 11:42:18 +0330

    swagger first config

--- app/routes/api/index.js
+++ app/routes/api/index.js
@@ -1,6 +1,25 @@
 const homeController = require('../../http/controllers/api/home.controller');
 
 const router = require('express').Router();
+/**
+ * @swagger
+ * tags:
+ *  name: IndexPage
+ *  description: Index Page Route And Data
+ */
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: Index Route
+ *      tags: [IndexPage]
+ *      description: get all data for index page
+ *      responses:
+ *          200:
+ *              description: success
+ *          404:
+ *              description: not found
+ */
 
 router.get("/", homeController.indexPage);
 
--- app/server.js
+++ app/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const { AllRoutes } = require('./routes/router');
 const morgan = require('morgan');
 const createError = require('http-errors');
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 module.exports = class Application {
 
@@ -25,12 +27,28 @@ module.exports = class Application {
         this.#app.use(express.json());
         this.#app.use(express.urlencoded({ extended: true }));
         this.#app.use(express.static(path.join(__dirname, "..", "public")));
+        //? Swagger Config
+        this.#app.use("/api-doc", swaggerUI.serve, swaggerUI.setup(swaggerJsDoc({
+            swaggerDefinition: {
+                info: {
+                    title: "Store Project",
+                    version: "1.0.0",
+                    description: "A Store Restful API"
+                },
+                servers: [
+                    {
+                        url: "http://localhost:5000"
+                    }
+                ]
+            },
+            apis: ["./app/routes/**/*.js"]
+        })))
     }
 
     createServer() {
         const http = require('http');
         http.createServer(this.#app).listen(this.#PORT, () => {
-            console.log(`Server Is Up And Online On Port: ${ this.#PORT }`);     
+            console.log(`Server Is Up And Online On Port: ${ this.#PORT } \n http://localhost:5000`);     
         })
     }
 
